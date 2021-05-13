5e03960608562341ce817b0abc490d4575b232bd
DevangJain <70439930+DevangJain@users.noreply.github.com>
2021-05-13 19:00:24 +0530

    Update song.py

--- AlexaSongBot/modules/song.py
+++ AlexaSongBot/modules/song.py
@@ -30,7 +30,7 @@ async def song(client, message):
     if args.startswith(" "):
         await message.reply("Enter a song name. Check /help")
         return ""
-    status = await message.reply("🔎Searching song from YouTube 📺.. Please wait ⏳️  ")
+    status = await message.reply("Searching song 🎶 Please wait ⏳️  ")
     video_link = yt_search(args)
     if not video_link:
         await status.edit("Song not found ❌.")
